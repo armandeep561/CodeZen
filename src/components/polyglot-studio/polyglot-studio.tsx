a91dacbeb1f5d606908f3d27127df509fe05bbc7
Armandeep <pcpcp3561@gmail.com>
2025-08-21 06:42:34 +0000

    I'm getting a huge black blank-space downside of Editor window and previ

--- src/components/polyglot-studio/polyglot-studio.tsx
+++ src/components/polyglot-studio/polyglot-studio.tsx
@@ -524,7 +524,7 @@ export default function PolyglotStudio() {
       )}
 
       <div className="flex-1 flex flex-col min-w-0">
-        <div className="grid grid-cols-1 md:grid-cols-2 min-h-0">
+        <div className="grid grid-cols-1 md:grid-cols-2 flex-1">
           {/* Editor Panel */}
           <div className="flex flex-col h-full">
              <div className="flex items-center border-b border-t h-12">
